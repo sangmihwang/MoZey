e88465cc1f009de64a1d25e37b0214753050387a
ysang10 <ysang10@naver.com>
2023-09-07 13:49:55 +0900

    :heavy_plus_sign: Feat(develop): 파일 구조 생성

---
+++ backend/tenten/src/main/java/com/ssafy/tenten/TentenApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.tenten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TentenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TentenApplication.class, args);
+	}
+
+}
