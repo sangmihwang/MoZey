800eb82e315718b42fea12421fa13fe8442ed133
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2023-09-14 17:34:08 +0900

    Revert "Merge branch 'feature#9' into 'develop'"
    This reverts commit 0537d97dbfb3f2db64075bae1917023cd6d70f91, reversing
    changes made to e18756b1a93d748628b13995ab737a7ea115753f.

--- backend/tenten/src/main/java/com/ssafy/tenten/api/repository/QuizSolveRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.tenten.api.repository;
-
-import com.ssafy.tenten.domain.QuizSolve;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizSolveRepository extends JpaRepository<QuizSolve, Long> {
-}
