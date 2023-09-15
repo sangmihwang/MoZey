7e19a6791de5e95003907b280f6ccae2be2958ca
6531z <6531z@naver.com>
2023-09-15 16:53:01 +0900

    ➕ Feat(feature#4): Cacjeable 적용

--- backend/tenten/src/main/java/com/ssafy/tenten/vo/Response/QuestionResponse.java
+++ backend/tenten/src/main/java/com/ssafy/tenten/vo/Response/QuestionResponse.java
@@ -3,15 +3,16 @@ package com.ssafy.tenten.vo.Response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.ssafy.tenten.domain.Question;
 import com.ssafy.tenten.vo.Request.QuestionRequest;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
+
+import java.io.Serializable;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL) // 널값은 전달하지 않는다
-public class QuestionResponse {
+public class QuestionResponse implements Serializable {
     private String qtnContent;
     private Character status;
     private String image;
