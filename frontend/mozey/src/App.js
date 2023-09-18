3be85b705dacb1cd88faa7a39317405743cd6a1e
bbangkku <qudrnr5770@gmail.com>
2023-09-18 17:56:51 +0900

    :heavy_plus_sign: Feat(feature#10): 알림 푸시 (FE)

--- frontend/mozey/src/App.js
+++ frontend/mozey/src/App.js
@@ -1,31 +1,12 @@
-import React, { Component } from "react";
-import firebase from "./firebase";
-import { messaging } from "./firebase";
+import { useEffect } from "react";
+import { fireStore } from "./firebase";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  componentDidMount() {
-    console.log("asdas");
-    console.log(messaging);
-    // const messaging = firebase.messaging();
-    messaging
-      .requestPermission()
-      .then(() => {
-        // 등록토큰받기
-        return messaging.getToken();
-      })
-      .then((token) => {
-        // 토큰 출력
-        console.log("Token : ", token);
-      })
-      .catch(() => {
-        console.log("error");
-      });
-  }
-  render() {
-    return <div>asdasdasdtest</div>;
-  }
+function App() {
+  useEffect(() => {
+    console.log(fireStore);
+  });
+
+  return <div className="App">{fireStore._databaseId.projectId}</div>;
 }
+
+export default App;
--- frontend/mozey/src/firebase.js
+++ frontend/mozey/src/firebase.js
@@ -1,5 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging";
+// Import the functions you need from the SDKs you need
+import {
+  initializeApp,
+  getApp,
+  getApps,
+} from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+import {
+  getMessaging,
+  getToken,
+} from "https://www.gstatic.com/firebasejs/9.20.0/firebase-messaging.js";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
   apiKey: "AIzaSyAPNKAqEr8kLRQfueq9kenMof56J4xLas0",
@@ -11,8 +23,10 @@ const firebaseConfig = {
   measurementId: "G-1FMV97F8NY",
 };
 const firebase = initializeApp(firebaseConfig);
-console.log(firebase);
-const messaging = getMessaging(firebaseConfig);
-console.log(messaging);
-// firebase.initializeApp(firebaseConfig);
-export { firebase, messaging };
+const appmessaging = getMessaging(firebase);
+const fireStore = getFirestore(firebase);
+const token = await getToken(appmessaging, {
+  key: process.env.REACT_APP_VAPID_KEY,
+});
+
+export { fireStore };
