fa92444ccd760410b8a6dd4a5179f34c7c91e71c
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2023-09-19 10:50:48 +0900

    :heavy_plus_sign: Feat(feature#15): 퀴즈, 퀴즈등록 머지를 위한 새 브랜치 생성

---
+++ backend/tenten/src/main/java/com/ssafy/tenten/api/service/QuizService.java
@@ -0,0 +1,13 @@
+package com.ssafy.tenten.api.service;
+
+import com.ssafy.tenten.dto.QuizDto;
+import com.ssafy.tenten.vo.Response.QuizResponse;
+
+import java.time.LocalDateTime;
+import java.util.List;
+public interface QuizService {
+    List<QuizResponse> getQuizzesByDate(LocalDateTime date);
+    void createQuiz(QuizDto quizDto);
+
+//    List<QuizDto> getAllQuizzes();
+}
