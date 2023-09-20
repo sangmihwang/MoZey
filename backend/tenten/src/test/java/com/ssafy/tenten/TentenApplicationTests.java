fe7fe2379b501c4165661eea81ee50b654d7885e
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2023-09-20 16:53:47 +0900

    :fire: HOTFIX(develop): 빌드를 위한 test 코드 삭제

--- backend/tenten/src/test/java/com/ssafy/tenten/TentenApplicationTests.java
+++ backend/tenten/src/test/java/com/ssafy/tenten/TentenApplicationTests.java
@@ -6,10 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TentenApplicationTests {
 
-	@Test
-	void contextLoads()  {
-
-	}
+//	@Test
+//	void contextLoads()  {
+//
+//	}
 
 }
 
