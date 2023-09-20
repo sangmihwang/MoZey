7b329e6471020a8e1443d0756fe7731f65d40dcd
MinWoongLee <dlalsdnd2@gmail.com>
2023-09-21 03:34:28 +0900

    :fire: HOTFIX(develop_FE): 임시페이지 생성

--- frontend/mozey/src/App.js
+++ frontend/mozey/src/App.js
@@ -1,8 +1,20 @@
 // import { useEffect, useState } from "react";
-import "./App.css";
 // import {} from "./config/firebase";
+import React from 'react'
+import './App.css'
+import Header from './components/Header'
+import Main from './components/Main';
+import Footer from './components/Footer'
+
 function App() {
-  return <div className="App">asd</div>;
+  return (
+    <div className="App">
+      {/* <Header />  */}
+      <Main/>
+      {/* <Footer/> */}
+      {/* 나머지 컴포넌트가 여기 위치할 것입니다. */}
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/mozey/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/Footer.css'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="tab">Tab 1</div>
+      <div className="tab">Tab 2</div>
+      <div className="tab">Tab 3</div>
+    </footer>
+  );
+}
+
+export default Footer;
