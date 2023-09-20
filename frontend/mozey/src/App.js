79ff0eb3c67968987cd91a1e9f6858bb1cdaecff
MinWoongLee <dlalsdnd2@gmail.com>
2023-09-21 02:12:21 +0900

    :fire: HOTFix(develop_FE) HTTP 서버 이슈로 firebase 파트 임시 주석처리

--- frontend/mozey/src/App.js
+++ frontend/mozey/src/App.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import "./App.css";
-import {} from "./config/firebase";
+// import {} from "./config/firebase";
 function App() {
   return <div className="App">asd</div>;
 }
