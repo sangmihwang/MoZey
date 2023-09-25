a58ab1a027d3e7a479199ab8fcb6c306b49aad6f
sangmihwang <01071184366@gmail.com>
2023-09-25 15:39:08 +0900

    ➕ Feat(feature#18): Main  화면 구현 완료

--- frontend/mozey/src/components/main/Main.jsx
+++ frontend/mozey/src/components/main/Main.jsx
@@ -9,29 +9,41 @@ const Main = () => {
   return (
     <S.Wrap>
       <S.Logo>
-        <div>
-          <img src={mozeyLogo} alt={"로고"} />
-        </div>
-        <div>
-          <img src={thinkerLogo} alt={"thinker"} />
-        </div>
+        <img src={mozeyLogo} alt={"로고"} />
       </S.Logo>
+      <S.ThinkerImage>
+        <img src={thinkerLogo} alt={"thinker"} />
+      </S.ThinkerImage>
     </S.Wrap>
   );
 };
 
 const S = {
   Wrap: styled.div`
+    display: flex;
+    flex-direction: column;
     width: 100%;
     height: 100%;
-    background: ${({ theme }) => theme.color.background};
+    background: linear-gradient(
+      ${({ theme }) => theme.color.background2} 28%,
+      ${({ theme }) => theme.color.background} 28%
+    );
+
     text-align: center;
+    padding-top: 100px;
+    align-items: center;
   `,
   Logo: styled.div`
-    display: inline-block;
-    > div {
-      display: block;
-      margin: 30px auto;
+    height: 160px;
+    margin: 30px auto;
+  `,
+  ThinkerImage: styled.div`
+    align-self: center;
+    width: 300px;
+    margin: 30px auto;
+    img {
+      width: 100%;
+      height: auto;
     }
   `,
 };
