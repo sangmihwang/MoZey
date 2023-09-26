1fb3ca84cf333a156c8bea7c11cce62b947c3250
sangmihwang <01071184366@gmail.com>
2023-09-26 15:25:50 +0900

    ➕ Feat(feature#125): TopTab 수정

---
+++ frontend/mozey/src/components/message/MessageContent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const MessageContent = () => {
+  return;
+  <S.Wrap></S.Wrap>;
+};
+
+const S = {
+  Wrap: styled.div``,
+};
+
+export default MessageContent;
--- frontend/mozey/src/components/message/MessageList.jsx
+++ frontend/mozey/src/components/message/MessageList.jsx
@@ -5,13 +5,27 @@ const MessageList = () => {
   return (
     <S.Wrap>
       <S.MessageBox></S.MessageBox>
+      <S.MessageBox></S.MessageBox>
+      <S.MessageBox></S.MessageBox>
+      <S.MessageBox></S.MessageBox>
+      <S.MessageBox></S.MessageBox>
     </S.Wrap>
   );
 };
 
 const S = {
-  Wrap: styled.div``,
-  MessageBox: styled.div``,
+  Wrap: styled.div`
+    background-color: ${({ theme }) => theme.color.background};
+    padding: 18px;
+  `,
+  MessageBox: styled.div`
+    background-color: ${({ theme }) => theme.color.white};
+    width: 100%;
+    height: 108px;
+    margin: 8px auto 18px;
+    border-radius: 10px;
+    box-shadow: ${({ theme }) => theme.shadow.card};
+  `,
 };
 
 export default MessageList;
