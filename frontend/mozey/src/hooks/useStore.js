9f4f87c795a907387921ef3cc212310c2074f0ee
sangmihwang <sangmihwangz@gmail.com>
2023-10-02 18:25:45 +0900

    ➕ Feat(feature#28): 투표화면 구현 중

--- frontend/mozey/src/hooks/useStore.js
+++ frontend/mozey/src/hooks/useStore.js
@@ -1,12 +1,5 @@
 import { create } from "zustand";
 
-// 예시
-
-// export const textChannelsState = create(set => ({
-//     textChannels: [],
-//     setTextChannels: data => set(state => ({ textChannels: data })),
-// }))
-
 export const candiChangeState = create((set) => ({
   isCandiChangeOpen: false,
   toggleCandiChangeOpen: () =>
