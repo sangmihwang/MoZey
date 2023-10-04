f4b7f6694ecdfb718969f71aa4b56439242010be
bbangkku <qudrnr5770@gmail.com>
2023-10-04 20:31:20 +0900

    ➕ Feat(feature#38): firebase 코드 수정

--- frontend/mozey/src/pages/exchange/Exchange.jsx
+++ frontend/mozey/src/pages/exchange/Exchange.jsx
@@ -2,12 +2,30 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import * as components from "components";
 import useStore from "store";
-
+import voteAPI from "../../api/voteAPI";
 const Exchange = () => {
+  const qtnId = 1;
+  const userId = 50;
+  const chosen = 49;
+
+  const handleClick = async () => {
+    const requestData = {
+      qtnId: qtnId,
+      userId: userId,
+      chosen: chosen,
+    };
+    try {
+      const response = await voteAPI.postVoteNotification(requestData);
+      console.log("Notification Data:", response.data);
+    } catch (error) {
+      console.log("에러", error);
+    }
+  };
   return (
     <div>
       <S.Wrap>
         <components.Exchange></components.Exchange>
+        <button onClick={handleClick}>임시버튼</button>
       </S.Wrap>
     </div>
   );
