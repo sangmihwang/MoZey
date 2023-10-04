54cef4b718cbb5f6a477fa402a9d62e4700ad870
sangmihwang <01071184366@gmail.com>
2023-10-04 14:49:47 +0900

    :heavy_plus_sign: Feat(feature/#37): 뉴스퀴즈 화면 수정 완료

--- frontend/mozey/src/components/news/NewsSection.jsx
+++ frontend/mozey/src/components/news/NewsSection.jsx
@@ -1,25 +1,25 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import axios from 'axios';
+import axios from "axios";
 
 const NewsSection = () => {
   const [showMore, setShowMore] = useState(false);
   // const title = '뉴욕증시 상승 유지…”인플레이션” 논쟁에 지쳐가는 금융시장'
   // const script = `
   // 미국 8월 물가지표가 발표됐다. 8월 소비자물가지수(CPI)는 전년 동월 대비 3.7% 상승한 것으로 나타났다.
-  // 지난달 3.2% 상승보다 크게 확대됐으며 시장 전망치인 3.6%를 소폭 상회했다. 반면, 에너지와 식료품을 제외한 
-  // 근원CPI는 전년 동기 대비 4.3% 올랐다. 이는 전월의 4.7%에 보다 하락한 것이다. 즉, 8월 한달 동안 국제유가와 농산물 등 
+  // 지난달 3.2% 상승보다 크게 확대됐으며 시장 전망치인 3.6%를 소폭 상회했다. 반면, 에너지와 식료품을 제외한
+  // 근원CPI는 전년 동기 대비 4.3% 올랐다. 이는 전월의 4.7%에 보다 하락한 것이다. 즉, 8월 한달 동안 국제유가와 농산물 등
   // 원자재 가격의 상승이 가장 큰 원인으로 작용했음을 증명했다. 연준(Fed)에 혼동된 시그널을 줄 것으로 보인다.
-  // 시장 전문가들은 이번 보고서가 연준 정책변화에는 큰 영향을 주지 않을 것으로 판단한다. 다음주 19일 시작하는 FOMC 
+  // 시장 전문가들은 이번 보고서가 연준 정책변화에는 큰 영향을 주지 않을 것으로 판단한다. 다음주 19일 시작하는 FOMC
   // 회의 회의에서 이를 반영하여 기준금리 인상이 이루어질 수 있다는 의견이다.  시카고상품거래소(CME)의 페드워치(FedWatch)
   // 미국 8월 물가지표가 발표됐다. 8월 소비자물가지수(CPI)는 전년 동월 대비 3.7% 상승한 것으로 나타났다.
-  // 지난달 3.2% 상승보다 크게 확대됐으며 시장 전망치인 3.6%를 소폭 상회했다. 반면, 에너지와 식료품을 제외한 
-  // 근원CPI는 전년 동기 대비 4.3% 올랐다. 이는 전월의 4.7%에 보다 하락한 것이다. 즉, 8월 한달 동안 국제유가와 농산물 등 
+  // 지난달 3.2% 상승보다 크게 확대됐으며 시장 전망치인 3.6%를 소폭 상회했다. 반면, 에너지와 식료품을 제외한
+  // 근원CPI는 전년 동기 대비 4.3% 올랐다. 이는 전월의 4.7%에 보다 하락한 것이다. 즉, 8월 한달 동안 국제유가와 농산물 등
   // 원자재 가격의 상승이 가장 큰 원인으로 작용했음을 증명했다. 연준(Fed)에 혼동된 시그널을 줄 것으로 보인다.
-  // 시장 전문가들은 이번 보고서가 연준 정책변화에는 큰 영향을 주지 않을 것으로 판단한다. 다음주 19일 시작하는 FOMC 
+  // 시장 전문가들은 이번 보고서가 연준 정책변화에는 큰 영향을 주지 않을 것으로 판단한다. 다음주 19일 시작하는 FOMC
   // 회의 회의에서 이를 반영하여 기준금리 인상이 이루어질 수 있다는 의견이다.  시카고상품거래소(CME)의 페드워치(FedWatch)
   // `;
-  
+
   // const img_path = 'https://d18-invdn-com.investing.com/content/pic7b07617d73e0756c52d7be4e5623d842.png'
 
   const [newsData, setNewsData] = useState([]);
@@ -29,24 +29,26 @@ const NewsSection = () => {
       try {
         const currentDate = new Date();
         let dateToFetch;
-  
+
         if (currentDate.getHours() >= 12) {
           // 12:00 이후라면 오늘 날짜로 조회
-          dateToFetch = currentDate.toISOString().split('T')[0];
+          dateToFetch = currentDate.toISOString().split("T")[0];
         } else {
           // 12:00 이전이라면 어제 날짜로 조회
           currentDate.setDate(currentDate.getDate() - 1);
-          dateToFetch = currentDate.toISOString().split('T')[0];
+          dateToFetch = currentDate.toISOString().split("T")[0];
         }
-  
-        const response = await axios.get(`https://j9a510.p.ssafy.io/api/news?date=${dateToFetch}`);
+
+        const response = await axios.get(
+          `https://j9a510.p.ssafy.io/api/news?date=${dateToFetch}`
+        );
         setNewsData(response.data);
-        console.log('뉴스데이터: ', response.data)
+        console.log("뉴스데이터: ", response.data);
       } catch (error) {
         console.error("Error fetching news by date:", error);
       }
     };
-  
+
     fetchData();
   }, []);
 
@@ -54,7 +56,7 @@ const NewsSection = () => {
     <S.Wrap $isExpanded={showMore}>
       <S.Container $isExpanded={showMore}>
         <ul>
-          {newsData.map(news => (
+          {newsData.map((news) => (
             <S.NewsItem key={news.newsId}>
               <S.NewsTitle>{news.title}</S.NewsTitle>
               <S.NewsImage src={news.imageUrl} alt="News Thumbnail" />
@@ -77,18 +79,19 @@ const S = {
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: 0 3%;  // 내부 좌우 여백
-    height: ${({ $isExpanded }) => ($isExpanded ? "auto" : "calc(100vh - 108px - 73px)")};
-    background: ${({ theme }) => theme.color.background}
+    padding: 0 3%; // 내부 좌우 여백
+    height: ${({ $isExpanded }) =>
+      $isExpanded ? "auto" : "calc(100vh - 108px - 73px)"};
+    background: ${({ theme }) => theme.color.background};
   `,
   Container: styled.div`
     background: ${({ theme }) => theme.color.white};
     width: 100%;
     border-radius: 10px;
-    margin-top: 10px;
+    margin: 20px;
     display: flex;
     flex-direction: column;
-    padding: 3% 3%;  // 내부 상하, 좌우 여백
+    padding: 3% 3%; // 내부 상하, 좌우 여백
     box-shadow: 0 4px 4px rgb(0, 0, 0, 0.25);
     overflow-y: auto;
     max-height: ${({ $isExpanded }) => ($isExpanded ? "100%" : "547px")};
@@ -97,18 +100,18 @@ const S = {
   NewsItem: styled.li`
     display: flex;
     flex-direction: column;
+    padding: 8px;
   `,
   NewsImage: styled.img`
     width: 100%;
     height: auto;
-    margin-bottom: 10px;
+    margin: 10px 0;
     border-radius: 8px;
   `,
   NewsTitle: styled.h3`
     font-size: 20px;
-    font-weight: bold;
-    color: #040404;
-    margin-bottom: 10px;
+    font-weight: 900;
+    margin: 10px 0;
     line-height: ${({ theme }) => theme.lineheight.title1};
   `,
   NewsContent: styled.p`
--- frontend/mozey/src/components/news/QuizSection.jsx
+++ frontend/mozey/src/components/news/QuizSection.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import axios from 'axios';
+import axios from "axios";
 import quizimg from "../../assets/images/icon-question-default.png";
-
+import { GrCheckbox, GrCheckboxSelected } from "react-icons/gr";
 const QuizSection = () => {
   const [quizzes, setQuizzes] = useState([]);
   const [currentQuizIndex, setCurrentQuizIndex] = useState(0);
@@ -17,18 +17,20 @@ const QuizSection = () => {
         let dateToFetch;
 
         if (currentDate.getHours() >= 12) {
-          dateToFetch = currentDate.toISOString().split('T')[0];
+          dateToFetch = currentDate.toISOString().split("T")[0];
         } else {
           currentDate.setDate(currentDate.getDate() - 1);
-          dateToFetch = currentDate.toISOString().split('T')[0];
+          dateToFetch = currentDate.toISOString().split("T")[0];
         }
 
-        const response = await axios.get(`https://j9a510.p.ssafy.io/api/quiz?date=${dateToFetch}`);
-        
+        const response = await axios.get(
+          `https://j9a510.p.ssafy.io/api/quiz?date=${dateToFetch}`
+        );
+
         // Randomly select 5 quizzes
         const shuffledQuizzes = response.data.sort(() => 0.5 - Math.random());
         const selectedQuizzes = shuffledQuizzes.slice(0, 5);
-        
+
         setQuizzes(selectedQuizzes);
       } catch (error) {
         console.error("퀴즈데이터 안불러와짐", error);
@@ -51,7 +53,7 @@ const QuizSection = () => {
     setShowModal(false);
     setSelectedChoice(null);
     if (isCorrect) {
-      setCurrentQuizIndex(prevIndex => prevIndex + 1);
+      setCurrentQuizIndex((prevIndex) => prevIndex + 1);
     }
   };
 
@@ -62,11 +64,23 @@ const QuizSection = () => {
           <S.Modal>
             <S.ModalContent isCorrect={isCorrect}>
               <S.ModalText>
-                {isCorrect ? "정답입니다!" : "틀렸습니다! 다시 한번 풀어보세요!"}
+                {isCorrect ? (
+                  <>
+                    정답입니다!
+                    <br />
+                    포인트가 적립되었습니다!
+                  </>
+                ) : (
+                  <>
+                    오답입니다!
+                    <br />
+                    다시 한번 풀어보세요!
+                  </>
+                )}
               </S.ModalText>
             </S.ModalContent>
             <S.ModalButton onClick={moveToNextQuiz}>
-              {isCorrect ? "다음 문제로" : "닫기"}
+              {isCorrect ? "다음 문제" : "확인"}
             </S.ModalButton>
           </S.Modal>
         </S.ModalOverlay>
@@ -77,17 +91,27 @@ const QuizSection = () => {
             <S.QuizImage src={quizimg} alt="Quiz Thumbnail" />
             <S.QuizTitle>{quizzes[currentQuizIndex].question}</S.QuizTitle>
             <S.ChoicesContainer>
-              {Object.entries(quizzes[currentQuizIndex].multipleChoice).map(([key, choice]) => (
-                <S.QuizChoice 
-                  key={key} 
-                  selected={selectedChoice === choice}
-                  onClick={() => setSelectedChoice(choice)}
-                >
-                  {choice}
-                </S.QuizChoice>
-              ))}
+              {Object.entries(quizzes[currentQuizIndex].multipleChoice).map(
+                ([key, choice]) => (
+                  <S.ChoiceContainer key={key}>
+                    {selectedChoice === choice ? (
+                      <S.StyledGrCheckboxSelected />
+                    ) : (
+                      <S.StyledGrCheckbox />
+                    )}
+                    <S.QuizChoice
+                      selected={selectedChoice === choice}
+                      onClick={() => setSelectedChoice(choice)}
+                    >
+                      {choice}
+                    </S.QuizChoice>
+                  </S.ChoiceContainer>
+                )
+              )}
             </S.ChoicesContainer>
-            <S.CheckAnswerButton onClick={() => checkAnswer(quizzes[currentQuizIndex].answer)}>
+            <S.CheckAnswerButton
+              onClick={() => checkAnswer(quizzes[currentQuizIndex].answer)}
+            >
               정답 확인하기
             </S.CheckAnswerButton>
           </>
@@ -106,61 +130,67 @@ const S = {
     align-items: center;
     justify-content: center;
     height: calc(100vh - 108px - 73px);
-    padding: 0 3%;  
+    padding: 0 20px;
     background: ${({ theme }) => theme.color.background};
   `,
   Container: styled.div`
     background: ${({ theme }) => theme.color.white};
     width: 100%;
     border-radius: 10px;
-    margin-top: 10px;
+    margin: 20px;
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: 3% 3%;
-    box-shadow: 0 4px 4px rgb(0, 0, 0, 0.25);
+    padding: 20px;
+    box-shadow: ${({ theme }) => theme.shadow.card};
     overflow-y: auto;
     flex: 1;
   `,
   QuizImage: styled.img`
     width: 74px;
     height: 84px;
-    margin-bottom: 10px;
+    margin: 20px;
     border-radius: 8px;
     object-fit: cover;
   `,
   QuizTitle: styled.h3`
-    font-size: 20px;
-    font-weight: bold;
-    color: #040404;
-    margin-bottom: 30px;
+    padding: 16px 20px 20px 20px;
+    font-size: ${({ theme }) => theme.fontsize.title2};
+    font-weight: 700;
+    color: ${({ theme }) => theme.color.black};
+    line-height: ${({ theme }) => theme.lineheight.quiztitle};
   `,
   ChoicesContainer: styled.div`
     width: 100%;
+    padding: 0 20px 20px 40px;
     display: flex;
     flex-direction: column;
-    gap: 15px;
+  `,
+  ChoiceContainer: styled.div`
+    display: flex;
+    align-items: center;
   `,
   QuizChoice: styled.button`
-    background-color: ${({ selected }) => (selected ? "#f0f0f0" : "#ffffff")};
-    border: 1px solid #e0e0e0;
-    color: #333;
-    font-size: 18px;
+    text-align: left;
+    color: ${({ selected, theme }) =>
+      selected ? theme.color.blue : theme.color.black};
+    font-size: ${({ theme }) => theme.fontsize.title3};
     padding: 10px 20px;
     border-radius: 5px;
     transition: background-color 0.3s;
 
     &:hover {
-      background-color: #e0e0e0;
+      color: ${({ theme }) => theme.color.blue};
     }
   `,
   CheckAnswerButton: styled.button`
     background-color: ${({ theme }) => theme.color.red};
-    color: #333;
+    color: ${({ theme }) => theme.color.white};
     font-size: 16px;
-    padding: 8px 16px;
+    padding: 12px 30px;
     margin-top: 15px;
-    border-radius: 5px;
+    border-radius: 10px;
+    box-shadow: ${({ theme }) => theme.shadow.card};
     transition: background-color 0.3s;
 
     &:hover {
@@ -173,13 +203,18 @@ const S = {
     left: 0;
     right: 0;
     bottom: 0;
-    background-color: rgba(0, 0, 0, 0.7);
+    background-color: rgba(0, 0, 0, 0.5);
     display: flex;
     align-items: center;
     justify-content: center;
   `,
   Modal: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     background-color: white;
+    margin: 24px;
     padding: 20px;
     border-radius: 10px;
   `,
@@ -194,24 +229,30 @@ const S = {
     height: auto;
   `,
   ModalText: styled.div`
-    font-size: 24px;
-    font-weight: bold;
-    color: ${({ isCorrect }) => (isCorrect ? "green" : "red")};
-    margin: 20px 0;
+    text-align: center;
+    font-size: ${({ theme }) => theme.fontsize.title2};
+    line-height: ${({ theme }) => theme.lineheight.quiztitle};
+    font-weight: 800;
+    margin: 20px;
   `,
   ModalButton: styled.button`
     padding: 10px 20px;
-    font-size: 18px;
-    background-color: #333;
+    font-size: ${({ theme }) => theme.fontsize.title4};
+    background-color: ${({ theme }) => theme.color.red};
     color: white;
-    border: none;
-    border-radius: 5px;
+    border-radius: 10px;
+    box-shadow: ${({ theme }) => theme.shadow.card};
     cursor: pointer;
     &:hover {
       background-color: #555;
     }
   `,
-  
+  StyledGrCheckbox: styled(GrCheckbox)`
+    color: ${({ theme }) => theme.color.black};
+  `,
+  StyledGrCheckboxSelected: styled(GrCheckboxSelected)`
+    color: ${({ theme }) => theme.color.blue};
+  `,
 };
 
 export default QuizSection;
