8faa1875ef59ce686cd5befe76bb9694e474d829
송채은 <sce9842@hanmail.net>
2023-10-05 21:15:27 +0900

    :bug: FIX(HOTFIX/voteSearch) : CandidatesSearch axios 오류 해결

--- frontend/mozey/src/components/vote/CandidatesSearch.jsx
+++ frontend/mozey/src/components/vote/CandidatesSearch.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import useStore from "../../store/userInfoStore";
+import axios from "axios";
 import styled from "styled-components";
 import * as components from "components";
 import { BiSearch } from "react-icons/bi";
