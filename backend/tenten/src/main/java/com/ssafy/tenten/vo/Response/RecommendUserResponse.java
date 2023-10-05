fd78e673f5468ff040efcfe5ebff293064bd48a3
6531z <6531z@naver.com>
2023-10-06 08:37:22 +0900

    📝 Docs(update): 추천 친구 불러오기 부분 쿼리dsl로 변경 , 친구 삭제 부분 수정

--- backend/tenten/src/main/java/com/ssafy/tenten/vo/Response/RecommendUserResponse.java
+++ backend/tenten/src/main/java/com/ssafy/tenten/vo/Response/RecommendUserResponse.java
@@ -1,6 +1,7 @@
 package com.ssafy.tenten.vo.Response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,11 +9,12 @@ import lombok.Getter;
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class RecommendUserResponse {
-    private final char gender;
+    private final Character gender;
     private final Long userId;
     private final String image;
     private final String name;
     private final String term;
     private final String campus;
     private final String group;
+
 }
