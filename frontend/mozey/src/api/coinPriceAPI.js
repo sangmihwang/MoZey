e4cb63dcbc8424fc0b181af283c5f75e01047c33
bbangkku <qudrnr5770@gmail.com>
2023-10-05 14:01:14 +0900

    ➕ Feat(feature#38): ChartData 코드 수정

--- frontend/mozey/src/api/coinPriceAPI.js
+++ frontend/mozey/src/api/coinPriceAPI.js
@@ -1,4 +1,5 @@
 import { axiosInstance } from "api";
+import axios from "axios";
 
 const coinPriceAPI = {
   // 투표 알림 받기
@@ -6,9 +7,12 @@ const coinPriceAPI = {
     axiosInstance.get("https://j9a510.p.ssafy.io:/api/coins/price"),
 
   // 코인 교환
-  exchangeCoin: (fromCoinName, toCoinName, minusCoinAmount, plusCoinAmount) =>
-    // console.log(fromCoinName, toCoinName, minusCoinAmount, plusCoinAmount),
-    axiosInstance.post(`/api/coins/exchange/`),
+  exchangeCoin: (data) =>
+    axios.post(`https://j9a510.p.ssafy.io:/api/coins/exchange/`, data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }),
 };
 
 export default coinPriceAPI;
