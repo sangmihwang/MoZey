aba92c9a219691a066f4c58d3b36c201ad7e8437
sangmihwang <01071184366@gmail.com>
2023-10-05 20:49:06 +0900

    :heavy_plus_sign: Feat(feature/#46): 마이페이지 수정

--- frontend/mozey/src/components/mypage/RecommendFriends.jsx
+++ frontend/mozey/src/components/mypage/RecommendFriends.jsx
@@ -1,203 +1,223 @@
 import { useEffect, useState } from "react";
 import useStore from "../../store/userInfoStore";
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from "react-router-dom";
 import React from "react";
 import styled from "styled-components";
-import axios from 'axios';
+import axios from "axios";
 import SearchModal from "./SearchFriends";
-import SearchImage from "assets/images/icon-searchFriends.png"
-import ProfileImage from "assets/images/icon-profileImg-default.svg"
+import SearchImage from "assets/images/icon-searchFriends.png";
+import ProfileImage from "assets/images/icon-profileImg-default.svg";
 
 const RecommendFriends = () => {
-	const userInfo = useStore((state) => state.User);
-	const [recommend, setRecommend] = useState([]);
-	const [modalOpen, setModalOpen] = useState(false);
+  const userInfo = useStore((state) => state.User);
+  const [recommend, setRecommend] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
 
-	useEffect(() => {
-		const getRecommend = async () => {
-			try {
-				const id = userInfo.id;
-				axios.get(`https://j9a510.p.ssafy.io/api/users/friends/recommend/${id}}`)
-					.then((data) => {
-						if (data.data.message === "친구 조회 완료") {
-							setRecommend(data.data.data);
-							console.log("추천 친구 조회", data.data.data);
-						}
-					})
-			} catch (e) {
-				console.log(e);
-			}
-		}
-		getRecommend();
-	}, []);
+  useEffect(() => {
+    const getRecommend = async () => {
+      try {
+        const id = userInfo.id;
+        axios
+          .get(`https://j9a510.p.ssafy.io/api/users/friends/recommend/${id}}`)
+          .then((data) => {
+            if (data.data.message === "친구 조회 완료") {
+              setRecommend(data.data.data);
+              console.log("추천 친구 조회", data.data.data);
+            }
+          });
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getRecommend();
+  }, []);
 
-	const addFriend = (id) => {
-		try {
-			const senderId = userInfo.id;
-			axios.post(`https://j9a510.p.ssafy.io/api/users/friends/follow/${senderId}/${id}`)
-				.then((data) => {
-					if (data.data.message === "친구 추가 완료") {
-						alert("추가했습니다");
-						window.location.href = "/mypage";
-					}
-				})
-		} catch (e) {
-			console.log(e);
-		}
-	}
+  const addFriend = (id) => {
+    try {
+      const senderId = userInfo.id;
+      axios
+        .post(
+          `https://j9a510.p.ssafy.io/api/users/friends/follow/${senderId}/${id}`
+        )
+        .then((data) => {
+          if (data.data.message === "친구 추가 완료") {
+            alert("추가했습니다");
+            window.location.href = "/mypage";
+          }
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-	const leaveModalOpen = async () => {
-		setModalOpen(true);
-	}
+  const leaveModalOpen = async () => {
+    setModalOpen(true);
+  };
 
-	const leaveModalClose = async () => {
-		setModalOpen(false);
-	}
+  const leaveModalClose = async () => {
+    setModalOpen(false);
+  };
 
-	return (
-		<S.Wrap>
-			<S.Top>
-				<S.Title>
-					<h2>추천 친구들</h2>
-				</S.Title>
-				<S.Search>
-					<button>
-						<img src={SearchImage} alt="search" />
-						<a onClick={() => leaveModalOpen()}>친구검색</a>
-					</button>
-				</S.Search>
-			</S.Top>
-			<S.Container>
-				<ul>
-					{recommend.map(friend => (
-						<S.Friend key={friend.userId}>
-							<S.FriendProfileBox>
-								{friend.image === null || !friend.image
-									? (<img src={ProfileImage} alt="profile" />)
-									: (<img src={friend.image} alt="profile" />)
-								}
-							</S.FriendProfileBox>
-							<S.FriendInfo>
-								<h2>{friend.name}</h2>
-								{friend.campus && friend.term && friend.group && (
-									<h4>{friend.campus}캠퍼스 | {friend.term}기 | {friend.group}반</h4>
-								)}
-							</S.FriendInfo>
-							<S.FriendAdd>
-								<button onClick={() => addFriend(friend.userId)}>추가</button>
-							</S.FriendAdd>
-						</S.Friend>
-					))}
-				</ul>
-			</S.Container>
-			{modalOpen && (
-				<SearchModal open={modalOpen} close={leaveModalClose}></SearchModal>
-			)}
-		</S.Wrap>
-	);
+  return (
+    <S.Wrap>
+      <S.Top>
+        <S.Title>
+          <h2>추천 친구들</h2>
+        </S.Title>
+        <S.Search>
+          <button>
+            <img src={SearchImage} alt="search" />
+            <a onClick={() => leaveModalOpen()}>검색</a>
+          </button>
+        </S.Search>
+      </S.Top>
+      <S.Container>
+        <ul>
+          {recommend.map((friend) => (
+            <S.Friend key={friend.userId}>
+              <S.FriendProfileBox>
+                {friend.image === null || !friend.image ? (
+                  <img src={ProfileImage} alt="profile" />
+                ) : (
+                  <img src={friend.image} alt="profile" />
+                )}
+              </S.FriendProfileBox>
+              <S.FriendInfo>
+                <h2>{friend.name}</h2>
+                {friend.campus && friend.term && friend.group && (
+                  <h4>
+                    {friend.campus}캠퍼스 | {friend.term}기 | {friend.group}반
+                  </h4>
+                )}
+              </S.FriendInfo>
+              <S.FriendAdd>
+                <button onClick={() => addFriend(friend.userId)}>추가</button>
+              </S.FriendAdd>
+            </S.Friend>
+          ))}
+        </ul>
+      </S.Container>
+      {modalOpen && (
+        <SearchModal open={modalOpen} close={leaveModalClose}></SearchModal>
+      )}
+    </S.Wrap>
+  );
 };
 
 const S = {
-	Wrap: styled.div`
-	margin-top: 20px;
-	width: 100%;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	`,
-	Top: styled.div`
-		width: 87.7%;
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-	`,
-	Title: styled.div`
-	font-size: 16px;
-	font-weight: bold;
-	color: #040404;
-    `,
-	Search: styled.div`
-		> button {
-		background-color: ${({ theme }) => theme.color.white};
-		color: ${({ theme }) => theme.color.lightgray};
-		box-shadow: ${({ theme }) => theme.shadow.card};
-		border-radius: 10px;
-		width: 83px;
-		height: 25px;
-		font-size: 14px;
-		font-weight: bold;
-		display: flex;
+  Wrap: styled.div`
+    margin-top: 20px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
     align-items: center;
-    justify-content: center;
-		}
-		> button:hover {
-    	background: #FBF7EF;
-  	}
-	`,
-	Container: styled.div`
-		background: ${({ theme }) => theme.color.white};
+  `,
+  Top: styled.div`
     width: 87.7%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  `,
+  Title: styled.div`
+    display: flex;
+    flex-direction: column;
+    font-size: 16px;
+    font-weight: bold;
+    color: #040404;
+  `,
+  Search: styled.div`
+    > button {
+      background-color: ${({ theme }) => theme.color.yellow};
+      color: ${({ theme }) => theme.color.lightgray};
+      box-shadow: ${({ theme }) => theme.shadow.card};
+      border-radius: 10px;
+      width: 100%;
+      height: 100%;
+      font-size: 14px;
+      font-weight: bold;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 4px 8px;
+    }
+    > button:hover {
+      background-color: ${({ theme }) => theme.color.white};
+      border: 1px solid ${({ theme }) => theme.color.yellow};
+      color: ${({ theme }) => theme.color.yellow};
+    }
+    > button > img {
+      margin: 2px;
+      :hover {
+        color: ${({ theme }) => theme.color.yellow};
+      }
+    }
+  `,
+  Container: styled.div`
+    background: ${({ theme }) => theme.color.white};
+    width: 90%;
     border-radius: 10px;
-		margin-top: 10px;
+    margin-top: 10px;
     display: flex;
     flex-direction: column;
     padding: 3% 7% 3%;
     box-shadow: ${({ theme }) => theme.shadow.card};
     overflow-y: auto;
-		max-height: 250px;
+    max-height: 250px;
+    min-height: 50px;
 
-		> ul > li {
-			width: 100%;
-		}
-    `,
-	Friend: styled.div`
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-		justify-content: space-between;
-		margin: 5%;
+    > ul > li {
+      width: 100%;
+    }
   `,
-	FriendProfileBox: styled.div`
-	width: 12yw;
-	height: 12vw;
-	background: ${({ theme }) => theme.color.background};
-	border-radius: 70%;
-	overflow: hidden;
-	> img {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-	}
-	`,
-	FriendInfo: styled.div`
-		text-align: left;
-		width: 60%;
-		
-		> h2 {
-			font-size: 16px;
-			font-weight: bold;
-			color: #040404;
-		}
-		> h4 {
-			color: ${({ theme }) => theme.color.darkgray};;
-			font-size: 12px;
-			font-weight: bold;
-			margin-top: 5px;
-		}
-	`,
-	FriendAdd: styled.div`
+  Friend: styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin: 5%;
+  `,
+  FriendProfileBox: styled.div`
+    width: 12yw;
+    height: 12vw;
+    background: ${({ theme }) => theme.color.background};
+    border-radius: 70%;
+    overflow: hidden;
+    > img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  `,
+  FriendInfo: styled.div`
+    text-align: left;
+    width: 60%;
 
-	> button {
-		background-color: ${({ theme }) => theme.color.blue};
-		color: ${({ theme }) => theme.color.white};
-		border-radius: 10px;
-		width: 40px;
-		height: 20px;
-		font-size: 12px;
-		font-weight: bold;
-		margin: auto;
-	}
-	`,
+    > h2 {
+      font-size: 16px;
+      font-weight: bold;
+      color: #040404;
+    }
+    > h4 {
+      color: ${({ theme }) => theme.color.darkgray};
+      font-size: 12px;
+      font-weight: bold;
+      margin-top: 5px;
+    }
+  `,
+  FriendAdd: styled.div`
+    > button {
+      background-color: ${({ theme }) => theme.color.blue};
+      color: ${({ theme }) => theme.color.white};
+      border-radius: 10px;
+      width: 40px;
+      height: 20px;
+      font-size: 12px;
+      font-weight: bold;
+      margin: auto;
+    }
+  `,
 };
 
 export default RecommendFriends;
